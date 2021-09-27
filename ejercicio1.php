1dc411bb9d04801709fc2b0c6f939a257adb631d
lexm25 <alejandro.martin.alvarez.al@iespoligonosur.org>
2021-09-27 12:09:49 +0200

    Ejercicio1 php

---
+++ ejercicio1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nombre = 'Alejandro Martín Álvarez';
+        $poblacion = 'Los Palacios y Villafranca';
+        $edad = 21;
+
+        echo "Mi nombre es",$nombre,"tengo",$edad, "años de edad y mi localidad es", $poblacion;
+        echo "<p></p>";
+        printf("Mi nombre es %s, tengo %d años de edad y mi localidad es %s",$nombre,$edad,$poblacion);
+    ?>
+</body>
+</html>
+
