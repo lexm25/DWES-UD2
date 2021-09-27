b3e720c4644bb80d65cfe10d5569d1f1ae458cbd
lexm25 <alejandro.martin.alvarez.al@iespoligonosur.org>
2021-09-27 12:12:03 +0200

    Ejercicio1 php

--- ejercicio1.php
+++ ejercicio1.php
@@ -12,7 +12,7 @@
         $poblacion = 'Los Palacios y Villafranca';
         $edad = 21;
 
-        echo "Mi nombre es",$nombre,"tengo",$edad, "años de edad y mi localidad es", $poblacion;
+        echo "Mi nombre es ",$nombre," tengo ",$edad, " años de edad y mi localidad es ", $poblacion;
         echo "<p></p>";
         printf("Mi nombre es %s, tengo %d años de edad y mi localidad es %s",$nombre,$edad,$poblacion);
     ?>
